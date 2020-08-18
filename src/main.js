f43a3b073798bbe6000c0764345b127f04334f4e
83742368@qq.com <83742368@qq.com>
2020-08-18 15:13:07 +0800

    删除无用文件

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ import router from './router'
 import store from './store'
 import filters from './utils/filter'
 Vue.config.productionTip = false
-Vue.prototype.$activeColor = '#976db9 '
+Vue.prototype.$activeColor = '#976db9'
 
-Object.keys(filters).forEach(key => {
+Object.keys(filters).forEach((key) => {
   Vue.filter(key, filters[key])
 })
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
